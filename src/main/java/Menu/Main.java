2667b87543966f847b9acec1b0de392941dc4915
Dayana Haro <harocarolina24@gmail.com>
2024-01-12 14:27:42 -0500

    menu

---
+++ src/main/java/Menu/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Menu;
+
+/**
+ *
+ * @author user
+ */
+public class Main {
+    public static void main(String[] args) {
+        PantallaPrincipal pantalla=new PantallaPrincipal();
+        pantalla.setVisible(true);
+    } 
+}
