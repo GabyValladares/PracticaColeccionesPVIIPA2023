1aa4c456008ef50fe3b35b003bdc57ed65dff298
Andy03062002 <christyan.moya234@ist17dejulio.edu.ec>
2024-01-15 11:55:38 -0500

    trabajo array del paquete array perdon por el atraso

---
+++ src/test/java/view/NewClass.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package view;
+
+/**
+ *
+ * @author Andy Moya
+ */
+public class NewClass {
+    public static void main (String[] args){
+        principal p = new principal();
+        p.setVisible(true);
+        
+    }
+    
+}
