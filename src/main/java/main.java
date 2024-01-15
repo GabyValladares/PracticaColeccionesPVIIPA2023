89314452e4d8c71c0862e19abd84727bd80fcfcf
Alen8815 <alen.pineda536@ist17dejulio.edu.ec>
2024-01-14 20:39:12 -0500

    uso de diferentes froms

---
+++ src/main/java/main.java
@@ -0,0 +1,19 @@
+
+import DIU.Menu1;
+import java.awt.Menu;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class main {
+    public static void main(String[] args) {
+        Menu1 menu=new Menu1();
+        menu.setVisible(true);
+    }
+}
