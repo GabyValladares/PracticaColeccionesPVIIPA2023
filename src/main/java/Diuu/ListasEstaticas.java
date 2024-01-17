5b3938e4afe54b25948f7b4eac1535738fea26bc
LeonardoMontalvo <axel.montalvo186@ist17dejulio.edu.ec>
2024-01-17 00:55:45 -0500

    diuu

--- src/main/java/Diuu/ListasEstaticas.java
+++ src/main/java/Diuu/ListasEstaticas.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JInternalFrame.java to edit this template
  */
-package diu;
+package Diuu;
 
 /**
  *
@@ -30,18 +30,17 @@ public class ListasEstaticas extends javax.swing.JInternalFrame {
         setIconifiable(true);
         setMaximizable(true);
         setResizable(true);
-        setTitle("Ejercicios con Arrays");
-        setToolTipText("");
+        setTitle("Colecciones ARRAYS");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 1222, Short.MAX_VALUE)
+            .addGap(0, 394, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 689, Short.MAX_VALUE)
+            .addGap(0, 274, Short.MAX_VALUE)
         );
 
         pack();
--- src/main/java/Diuu/Main.java
+++ src/main/java/Diuu/Main.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package diu;
+package Diuu;
 
 /**
  *
@@ -14,10 +14,8 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-                
-        Menu menu = new Menu ();
-        menu.setVisible(true);
+        MENU p = new MENU();
+        p.setVisible(true);
     }
     
 }
