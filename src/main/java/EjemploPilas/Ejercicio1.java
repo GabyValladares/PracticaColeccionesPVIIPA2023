36cb7343f34dbe0eed48c069c9b1b157a53f7584
LaraInsano2 <dilanlara1999@gmail.com>
2024-01-23 01:19:27 -0500

    FichaV1

--- src/main/java/EjemploPilas/Ejercicio1.java
+++ src/main/java/EjemploPilas/Ejercicio1.java
@@ -2,11 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package Pilas;
+package EjemploPilas;
 
 /**
  *
- * @author Usuario
+ * @author Dilan Lara
  */
 public class Ejercicio1 extends javax.swing.JFrame {
 
