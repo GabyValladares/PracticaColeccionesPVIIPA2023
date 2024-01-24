5e40c4da030c82f2bac5d0a4a6fe97efcd6c8677
Andy03062002 <christyan.moya234@ist17dejulio.edu.ec>
2024-01-23 22:38:32 -0500

    función que reciba una string y devuelve el string inviertido utilizando una Pila.

--- src/test/java/view/principal.java
+++ src/test/java/view/principal.java
@@ -37,7 +37,7 @@ public class principal extends javax.swing.JFrame {
         jMenuItem1 = new javax.swing.JMenuItem();
         jMenuItem2 = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
-        jMenu1 = new javax.swing.JMenu();
+        jMenuItem3 = new javax.swing.JMenuItem();
         jMenu3 = new javax.swing.JMenu();
         jMenu4 = new javax.swing.JMenu();
         menuFicha = new javax.swing.JMenu();
@@ -95,8 +95,13 @@ public class principal extends javax.swing.JFrame {
 
         jMenu2.setText("pilas");
 
-        jMenu1.setText("jMenu1");
-        jMenu2.add(jMenu1);
+        jMenuItem3.setText("jMenuItem3");
+        jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem3ActionPerformed(evt);
+            }
+        });
+        jMenu2.add(jMenuItem3);
 
         jMenuBar1.add(jMenu2);
 
@@ -189,6 +194,12 @@ public class principal extends javax.swing.JFrame {
      escritorio.add(listaprom1);
      listaprom1.show();
     }//GEN-LAST:event_jMenuItem2ActionPerformed
+
+    private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
+    CadenaInvertida cad = new CadenaInvertida();
+     escritorio.add(cad);
+     cad.show();
+    }//GEN-LAST:event_jMenuItem3ActionPerformed
     
     /**
      * @param args the command line arguments
@@ -227,13 +238,13 @@ public class principal extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     public static javax.swing.JDesktopPane escritorio;
-    private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenu jMenu3;
     private javax.swing.JMenu jMenu4;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem3;
     private javax.swing.JMenu menuFicha;
     private javax.swing.JMenu menuListas;
     private javax.swing.JMenuItem opDinamica;
