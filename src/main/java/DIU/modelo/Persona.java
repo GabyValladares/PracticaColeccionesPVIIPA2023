7eb8195a694f3253e629e00af2721bb679efee1e
SM581 <148915149+SM581@users.noreply.github.com>
2024-01-24 23:05:47 -0500

    "Práctica DIU_FICHAVEHICULAR"

---
+++ src/main/java/DIU/modelo/Persona.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DIU.modelo;
+
+/**
+ *
+ * @author 59399
+ */
+public class Persona {
+    public String nombres;
+
+    public Persona() {
+    }
+   
+
+    public Persona(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+    
+    
+    
+    
+}
