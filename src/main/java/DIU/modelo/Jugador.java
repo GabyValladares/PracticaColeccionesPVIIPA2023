3279c520a73fb9f7695a4d27843a6e184cd9de86
LaraInsano2 <dilanlara1999@gmail.com>
2024-01-31 15:15:22 -0500

    TareasDilanLara

---
+++ src/main/java/DIU/modelo/Jugador.java
@@ -0,0 +1,45 @@
+package DIU.modelo;
+
+/**
+ *
+ * @author DilanLara
+ */
+public class Jugador {
+
+    private String apellidos;
+    private int edad;
+    private String puesto;
+    private String nacionalidad;
+    private int goles;
+
+    public Jugador (){}
+
+    public Jugador(String apellidos, int edad, String puesto, String nacionalidad, int goles) {
+        this.apellidos = apellidos;
+        this.edad = edad;
+        this.puesto = puesto;
+        this.nacionalidad = nacionalidad;
+        this.goles = goles;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getPuesto() {
+        return puesto;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public int getGoles() {
+        return goles;
+    }
+
+}
